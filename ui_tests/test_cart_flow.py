1174dd65e745bb3e8a0f011305d307b2ac7801d3
Hyma <vits.19731a0585@gmail.com>
2026-04-16 21:50:47 +0530

    Update test_cart_flow.py

--- ui_tests/test_cart_flow.py
+++ ui_tests/test_cart_flow.py
@@ -9,26 +9,26 @@ def test_add_product_to_cart():
         browser = p.chromium.launch(headless=False)
         page = browser.new_page()
 
-        # Step 1: Open website
+        # Here we need to Open website
         home = HomePage(page)
         home.navigate()
 
-        # Step 2: Select product
+        # Then Select a product
         product_name = home.select_first_product()
 
-        # Step 3: Add to cart
+        # After that add an item into Add to cart
         product = ProductPage(page)
         product.add_to_cart()
 
-        # Step 4: Go to cart
+        # Check into item is added in cart so need to Go to cart
         product.go_to_cart()
 
-        # Step 5: Validate
+        # Finally we need to check here as item is added to cart and Validate the item as well
         cart = CartPage(page)
         assert cart.get_cart_item_name() == product_name
         assert cart.get_cart_count() == 1
 
-        # (Optional Bonus)
+        # Here we will check it as after remove an item from cart is it still visible or not(check cart is empty or not)
         cart.remove_item()
         assert cart.is_cart_empty()
 
